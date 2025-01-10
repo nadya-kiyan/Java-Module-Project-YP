47b774b8db980a5f5ed49aa66ba5534a718dbd7f
ndkn09 <nadyakiyan@gmail.com>
2025-01-10 17:41:13 +0300

    КИЯН - Проектная работа номер 1

--- src/main/java/Car.java
+++
@@ -1,9 +0,0 @@
-public class Car {
-    String name;
-    int speed;
-
-    Car (String carName, int carSpeed) {
-        name = carName;
-        speed = carSpeed;
-    }
-}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,6 @@
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String carName;
-        int carSpeed;
-        Scanner scanner = new Scanner(System.in);
-        Race carLeaderRace = new Race();
-
-        for (int i = 1; i <= 3; i++) {
-            System.out.println("Введите название машины № " + i);
-            carName = scanner.next();
-            while (true){
-                System.out.println("Введите скорость машины № " + i);
-                carSpeed = scanner.nextInt();
-                if (carSpeed >0 && carSpeed <=250) {
-                    break;
-                }
-                System.out.println("Неправильная скорость");
-            }
-            Car carObject = new Car(carName,carSpeed);
-            carLeaderRace.leaderCalculations(carObject);
-        }
-        System.out.println("Самая быстрая машина: " + carLeaderRace.carLeaderName);
-        scanner.close();
+        System.out.println("Hello world!");
     }
 }
